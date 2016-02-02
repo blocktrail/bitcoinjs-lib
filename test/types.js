9b3414c7e4ce7944b5c1f3c37aa02df2d2989960
Ruben de Vries <ruben@rubensayshi.com>
2016-02-02 13:06:16 +0100

    add failing test case for hash160/256 typeforce check

--- test/types.js
+++ test/types.js
@@ -2,6 +2,7 @@
 
 var assert = require('assert')
 var types = require('../src/types')
+var typeforce = require('typeforce')
 
 describe('types', function () {
   describe('BigInt/ECPoint', function () {
@@ -13,6 +14,26 @@ describe('types', function () {
     it('return false for bad types', function () {
       assert(!types.BigInt(new function NotABigInteger () {}))
       assert(!types.ECPoint(new function NotAPoint () {}))
+    })
+  })
+
+  describe('Buffer Hash160/Hash256', function () {
+    var buffer20byte = new Buffer((new Array(20 + 1)).join('00'), 'hex')
+    var buffer32byte = new Buffer((new Array(32 + 1)).join('00'), 'hex')
+
+    it('return true for correct size', function () {
+      assert(types.Hash160bit(buffer20byte))
+      assert(types.Hash256bit(buffer32byte))
+    })
+
+    it('return false for incorrect size', function () {
+      assert(!types.Hash160bit(buffer32byte), "32 byte buffer != Hash160")
+      assert(!types.Hash256bit(buffer20byte), "20 byte buffer != Hash256")
+    })
+
+    it('return true for oneOf', function () {
+      assert(typeforce(types.oneOf(types.Hash256bit, types.Hash160bit), buffer32byte), "Hash256 first")
+      assert(typeforce(types.oneOf(types.Hash160bit, types.Hash256bit), buffer32byte), "Hash160 first")
     })
   })
 })
