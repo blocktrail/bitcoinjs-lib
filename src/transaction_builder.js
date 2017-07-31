759fe5dc5bda7b2be2cd819ece5d26b492fbc758
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-07-31 11:09:50 +0200

    opt-in bitcoin-cash support in transaction_builder

--- src/transaction_builder.js
+++ src/transaction_builder.js
@@ -480,7 +480,16 @@ function TransactionBuilder (network, maximumFeeRate) {
   this.maximumFeeRate = maximumFeeRate || 1000
 
   this.inputs = []
+  this.bitcoinCash = false
   this.tx = new Transaction()
+}
+
+TransactionBuilder.prototype.enableBitcoinCash = function (setting) {
+  if (typeof setting === 'undefined') {
+    setting = true
+  }
+
+  this.bitcoinCash = setting
 }
 
 TransactionBuilder.prototype.setLockTime = function (locktime) {
@@ -694,11 +703,16 @@ TransactionBuilder.prototype.sign = function (vin, keyPair, redeemScript, hashTy
 
   // ready to sign
   var signatureHash
-  if (input.witness) {
-    signatureHash = this.tx.hashForWitnessV0(vin, input.signScript, input.value, hashType)
+  if (this.bitcoinCash) {
+    signatureHash = this.tx.hashForCashSignature(vin, input.signScript, witnessValue, hashType)
   } else {
-    signatureHash = this.tx.hashForSignature(vin, input.signScript, hashType)
+    if (input.witness) {
+      signatureHash = this.tx.hashForWitnessV0(vin, input.signScript, witnessValue, hashType)
+    } else {
+      signatureHash = this.tx.hashForSignature(vin, input.signScript, hashType)
+    }
   }
+
   // enforce in order signing of public keys
   var signed = input.pubKeys.some(function (pubKey, i) {
     if (!kpPubKey.equals(pubKey)) return false
